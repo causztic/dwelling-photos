6260fa91fa3e9cd26e3f65016eb5598552c85478
yaojie <limyaojie93@gmail.com>
2019-04-26 15:47:22 +0800

    add basic images

---
+++ src/components/ImageButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="select">
+    <img :src="src" @mouseover="hover = true" @mouseleave="hover = false" />
+    <h2 :class="{active: hover}">{{text}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageButton',
+  props: {
+    src: String,
+    text: String
+  },
+  data () {
+    return {
+      hover: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: 150px;
+  }
+  .select {
+    position: relative;
+  }
+  h2 {
+    display: none;
+  }
+  h2.active {
+    display: block;
+  }
+
+  img:hover {
+    opacity: 0.5;
+    cursor: pointer;
+  }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,30 +1,54 @@
 <template>
-  <div>
-    <h1>SIT Flats</h1>
-    <h2>Coming soon</h2>
+  <div class="container">
+    <img src="../assets/1.png" />
+    <div class="content">
+      <h1>SIT Flats</h1>
+      <p>
+        Exploring historical reasons for the creation of the Singapore Improvement Trust, with its policies
+        and architectural designs of its flats.
+      </p>
+      <div class="places">
+        <ImageButton :src="require('../assets/tb/1.jpeg')" text="Tiong Bahru" />
+        <ImageButton :src="require('../assets/d/1.png')" text="Dakota" />
+        <ImageButton :src="require('../assets/ks/1.jpeg')" text="Kampung Silat" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ImageButton from './ImageButton.vue'
+
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  components: { ImageButton }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.content {
+  width: 100%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.places {
+  display: flex;
+  flex-direction: column;
 }
-a {
-  color: #42b983;
+
+h1 {
+  width: 100%;
+  text-align: center;
+}
+p {
+  margin: 40px;
+}
+img {
+  height: 100%;
 }
 </style>
