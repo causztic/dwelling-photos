d6e0c73b9afb7a38806b8f1279e524c1dd631918
yaojie <limyaojie93@gmail.com>
2019-04-30 22:27:37 +0800

    add link to all photos

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -23,7 +23,7 @@
 import ImageButton from './ImageButton.vue'
 
 const originalTitle = 'SIT Flats'
-const originalContent = 'Exploring historical reasons for the creation of the Singapore Improvement Trust, with its policies and architectural designs of its flats. <br /><ol><li>Some images on the left can be scrolled</li><li>Click on any image below to start</li><li>Supporting commentary is given to provide insight</li><li>We also like to put out a disclaimer that we are not professional photographers but we try our best!'
+const originalContent = 'Exploring historical reasons for the creation of the Singapore Improvement Trust, with its policies and architectural designs of its flats. <br /><ol><li>Some images on the left can be scrolled</li><li>Click on any image below to start</li><li>Supporting commentary is given to provide insight</li><li>We also like to put out a disclaimer that we are not professional photographers but we try our best!<li>All of the photos we took are available <a href="https://drive.google.com/drive/folders/181DZrJQYRQalXI6n1V5f7IxvCCaB5NIs?usp=sharing">here</a></li>'
 
 export default {
   name: 'HelloWorld',
@@ -41,7 +41,7 @@ export default {
   props: {
     imageLengths: {
       type: Object,
-      default: () => ({ 'tb': 16, 'dc': 19, 'ks': 11 })
+      default: () => ({ 'tb': 14, 'dc': 19, 'ks': 11 })
     },
     contents: {
       type: Object,
@@ -50,7 +50,7 @@ export default {
           'dc': [
             'Dakota Crescent was built in 1958, modelled after British towns and adapted to suit the tropical climate. In 2017, it was announced by the government that the cluster with the central courtyard will be repurposed for civic and community usage.',
             'To be torn down to make way for new flats, the windows on the first floors are boarded up to prevent illegal entry.',
-            'The other courtyard that is not slated for preservation. It lies unkempt, but we still noticed a family coming over to play badminton after we were done photographing the area. A badminton court may not incite as much nostalgia as a sand-based playground it is often times found, whereas most sand-based playgrounds have been replaced with synthetic ground.',
+            'The other courtyard that is not slated for preservation. It lies unkempt, but we still noticed a family coming over to play badminton after we were done photographing the area. <br/><br/>A badminton court may not incite as much nostalgia as it is often times found, whereas most sand-based playgrounds have been replaced with synthetic ground and it\'s very rare to find one these days.',
             '(Scrollable) Some ornaments can still be seen hung on the ceiling. The brickwork is also substantially different from the HDB flats we see nowadays, with the brick pattern visible from afar.<br/><br/>A ramp has also been added into the structure retroactively, catering to the ageing population in the area.',
             '(Scrollable) Close up look at the different grille designs. Wear and tear from the elements would prompt the owners to replace their grilles.',
             'Different types of window installations can be seen here. We can also see the zinc plating under the bamboo pole holders, which is not found in HDB flats, as it rusts easily.',
@@ -69,7 +69,12 @@ export default {
             'The famous Dove playground to be preserved, as we imagine old playgrounds from the past would present a higher nostalgic factor to future homeowners. It is part of the central courtyard in between four blocks that form a square around it, and this concept is one of the very few remaining in Singapore.'
           ],
           'tb': [
-            '(Scrollable) Tiong Bahru is famous to tourists and Singaporeans alike for shophouses that sell exquisite products that may not be found elsewhere in Singapore.'
+            '(Scrollable) Tiong Bahru is famous to tourists and Singaporeans alike for shophouses that sell exquisite products that may not be found elsewhere in Singapore.',
+            'Shops are accommodating to tourists as shown from the multilingual sign in Chinese, Japanese and English (in the middle of the photo)',
+            'Tourists roaming around the area was a common sight, sporting sleeveless attires due to the hot and humid weather in Singapore',
+            '(Scrollable) One of the remaining SIT flats. Residents on the upper floors redesigned the windows, while the ones on the ground floor remained unchanged. This creates a dissonance in the overall aesthetics of the building',
+            'The backyard of the SIT flats. Clothing racks are installed but residents appear to not use them.',
+            'A single road separates condominiums (left), HDB Flats (center) as well as SIT flats (right).'
           ]
         }
       )
